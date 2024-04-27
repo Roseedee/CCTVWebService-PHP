17dcf972650136793ea987c4e9a0e4c043b5e8d8
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-04-28 02:01:07 +0700

    update image list in new-worksite

--- static/js/show-img.js
+++ static/js/show-img.js
@@ -9,10 +9,23 @@ document.getElementById('file-upload').addEventListener('change', function(event
     if (file.type.match('image.*')) {
       var reader = new FileReader();
       reader.onload = function(e) {
+
+        var divImg = document.createElement('div')
+        divImg.classList.add('img-list-item')
+
+        var iconDelete = document.createElement('img')
+        iconDelete.src = '../static/icon/trash-bin.png'
+
+        var btnDelete = document.createElement('div')
+        btnDelete.classList.add('img-btn-delete')
+
         var img = document.createElement('img');
         img.src = e.target.result;
-        img.style.height = '150px'; // Set maximum height for display
-        container.appendChild(img);
+
+        btnDelete.appendChild(iconDelete)
+        divImg.appendChild(img);
+        divImg.appendChild(btnDelete)
+        container.appendChild(divImg);
       }
       reader.readAsDataURL(file);
     }
