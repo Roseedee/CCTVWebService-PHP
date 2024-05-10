9b7fa635cbfd57e885cb11c5f7fb614ae6bba030
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-05-10 16:25:10 +0700

    fix bug auto delete image worksite when delete worksite_info

--- template/profile/action/delete-worksite.php
+++ template/profile/action/delete-worksite.php
@@ -3,11 +3,32 @@
     $worksite_id = $_GET['worksite-id'];
     $user_id = $_GET['user-id'];
 
-    $stmt = $con->prepare("DELETE FROM worksite WHERE worksite_id = :worksite_id");
-    $stmt->bindParam(':worksite_id', $worksite_id);
-    $stmt->execute();
+    // Delete associated images
+    $stmt_image_list = $con->prepare("SELECT * FROM worksite_image WHERE worksite_id = :worksite_id");
+    $stmt_image_list->bindParam(':worksite_id', $worksite_id);
+    $stmt_image_list->execute();
 
-    if ($stmt->rowCount() > 0) {
+    $worksite_img_list = $stmt_image_list->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($worksite_img_list as $image) {
+        $imageFilePath = '../../../uploads/worksite-img/'. $image['img_url'];
+        // echo $imageFilePath . '<br>';
+        if (file_exists($imageFilePath)) {
+            if (unlink($imageFilePath)) {
+                echo "Image deleted successfully.";
+            } else {
+                echo "Failed to delete image.";
+            }
+        }
+    }
+
+    // Delete worksite
+    $stmt_worksite = $con->prepare("DELETE FROM worksite WHERE worksite_id = :worksite_id");
+    $stmt_worksite->bindParam(':worksite_id', $worksite_id);
+    $stmt_worksite->execute();
+
+
+    if ($stmt_worksite->rowCount() > 0) {
         header('location: ../?user-id=' . $user_id);
 
     } else {
