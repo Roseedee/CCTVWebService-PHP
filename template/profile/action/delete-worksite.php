f273a766ea9c48c22b0f0efecf049029c2a50a4e
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-05-10 16:04:25 +0700

    fix bug after delete worksite

--- template/profile/action/delete-worksite.php
+++ template/profile/action/delete-worksite.php
@@ -1,13 +1,14 @@
 <?php
     require_once('../../action/dbconnect.php');
     $worksite_id = $_GET['worksite-id'];
+    $user_id = $_GET['user-id'];
 
     $stmt = $con->prepare("DELETE FROM worksite WHERE worksite_id = :worksite_id");
     $stmt->bindParam(':worksite_id', $worksite_id);
     $stmt->execute();
 
     if ($stmt->rowCount() > 0) {
-        header('location: ../');
+        header('location: ../?user-id=' . $user_id);
 
     } else {
         echo "Failed to delete worksite.";
