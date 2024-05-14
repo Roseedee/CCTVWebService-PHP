e3dbf3447aede7bcddefeda6430552257c0d1372
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-05-14 18:10:36 +0700

    add test notification UI

--- test/testui/static/js/new-worksite.js
+++ test/testui/static/js/new-worksite.js
@@ -68,7 +68,7 @@ document.getElementById('images-selector-add-worksite').addEventListener('change
 
 document.getElementById('user-search').addEventListener('input', function(event) {
     const user_search = document.getElementById('user-search').value
-    alert(user_search)
+    // alert(user_search)
 });
 
 document.getElementById('user-search').addEventListener('focus', function(event) {
@@ -76,57 +76,7 @@ document.getElementById('user-search').addEventListener('focus', function(event)
     user_list.display = 'block'
 });
 
-document.getElementById('user-search').addEventListener('blur', function(event) {
+document.getElementById('user-item').addEventListener('mouseleave', function(event) {
     const userList = document.getElementById('user-list').style;
     userList.display = 'none';
 });
-
-function onLoadUser() {
-    let user_list = document.getElementById('user-list');
-    user_list.innerHTML = '';
-    
-    var users = [
-        {
-            userId: "0001",
-            userName: "Roseedee Cehlaeh",
-            userImg: '1.jpg'
-        },
-        {
-            userId: "0002",
-            userName: "Solahudeen Cehlaeh",
-            userImg: '2.jpg'
-        },
-        {
-            userId: "0003",
-            userName: "Muhammad Cehlaeh",
-            userImg: '3.jpg'
-        }
-    ];
-    
-    users.forEach(user => {
-        const userDiv = document.createElement("div");
-        userDiv.classList.add("user-selector-item");
-        userDiv.dataset.userid = user.userId;
-
-        userDiv.innerHTML = `
-            <div class="about-user">
-                <p class="user-id m-0 text-muted">${user.userId}</p>
-                <p class="user-name m-0">${user.userName}</p>
-            </div>
-            <div class="user-img">
-                <img src="../static/image/test/${user.userImg}" alt="">
-            </div>
-        `;
-
-        userDiv.addEventListener('click', selectUserItem);
-
-        user_list.appendChild(userDiv);
-    });
-}
-
-function selectUserItem(event) {
-    const userId = event.currentTarget.dataset.userid;
-    alert('Selected user ID: ' + userId);
-}
-
-
