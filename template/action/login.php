b8317ea53f520d66c4c57bb091e2b08e0a6d0f69
6412013805017 <167610007+6412013805017@users.noreply.github.com>
2024-05-23 21:56:26 +0700

    คอมเม้นท์ที่ไฟล์ล็อกอิน

--- template/action/login.php
+++ template/action/login.php
@@ -3,25 +3,31 @@
     session_start();
 
     try {
+        //รับค่าจากฟอร์มล็อกอิน
         $username = $_POST['username'];
         $password = $_POST['password'];
 
+        //ป้องกัน sql แบบพื้นฐาน
         $username = stripslashes($username);
         $password = stripslashes($password);
         $username = htmlspecialchars($username);
         $password = htmlspecialchars($password);
 
+        //เช็ค username กับ password ในฐานข้อมูล
         $stmt = $con->prepare("SELECT * FROM account WHERE username=:username AND password=:password");
         $stmt->bindParam(':username', $username);
         $stmt->bindParam(':password', $password);
         $stmt->execute();
 
+        //เงื่อนไข สำหรับเช็ค uesrname กับ password ที่กรอกมามีหรือไม่ในฐานข้อมูล
         if ($stmt->rowCount() == 1) {
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
-            
+           
+            //เก็บค่าใน session
             $_SESSION['user-login-id'] = $user['user_id'];
             $_SESSION['user-acc-type'] = $user['account_type'];
 
+            //เช็คว่าเป็น admin หรือไม่
             if($user['account_type'] == 'admin') {
                 header("location: ../");
             }else {
