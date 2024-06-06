2addcd23d2a021cc662c69b4428b64196b0eb4bc
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-06-07 00:03:48 +0700

    fix bug

--- template/action/load-user.php
+++ template/action/load-user.php
@@ -3,18 +3,7 @@
 
     try {
         $stmt = $con->prepare("
-        SELECT 
-            u.user_id,
-            u.name_lastname,
-            u.img_type
-        FROM 
-            user u
-        JOIN 
-            account a ON u.user_id = a.user_id
-        WHERE 
-            a.account_type = 'customer'
-        GROUP BY 
-            u.user_id;
+            SELECT * FROM user;
         ");
         $stmt->execute();
     
